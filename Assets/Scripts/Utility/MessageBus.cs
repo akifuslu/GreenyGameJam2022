bf5a4ba634b7e20cced3e902e861184cbf6b520b
Akif Üslü <akifu.uslu@gmail.com>
2022-10-21 20:07:22 +0300

    Project initials

---
+++ Assets/Scripts/Utility/MessageBus.cs
@@ -0,0 +1,53 @@
+﻿using UniRx;
+using System;
+
+namespace Utility
+{
+
+    /// <summary>
+    /// Base class for custom game events.
+    /// Extend this class to implement your own events.
+    /// </summary>
+    public class GameEvent
+    {
+
+    }
+
+    /// <summary>
+    /// Proxy for UniRx event system.
+    /// </summary>
+    public static class MessageBus
+    {
+
+        /// <summary>
+        /// Publishes a new event of type T.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="evnt"></param>
+        public static void Publish<T>(T evnt) where T : GameEvent
+        {
+            MessageBroker.Default.Publish(evnt);
+        }
+
+        /// <summary>
+        /// Creates and subscribable to an event of type T.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static IObservable<T> OnEvent<T>() where T : GameEvent
+        {
+            return MessageBroker.Default.Receive<T>();
+        }
+
+
+        /// <summary>
+        /// Clears event subscriptions.
+        /// Note that this method called when a level is removed.
+        /// So it is not possible to have inter-level events at the moment.
+        /// </summary>
+        public static void ClearSubs()
+        {
+            //MessageBroker.Default.ClearSubs();
+        }
+    }
+}
